5992988b3760b827e0f74dd5cb554f397ef4af0f
Jungmin Kang <jmkang@goqual.com>
2021-12-01 09:25:20 +0900

    logout and redirect

--- src/components/Router.js
+++ src/components/Router.js
@@ -2,19 +2,33 @@ import React from "react";
 import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import Auth from "../routes/Auth";
 import Home from "../routes/Home";
+import Profile from "../routes/Profile";
+import Navigation from "./Navigation";
+import { Redirect } from "react-router";
 
 const AppRouter = ({ isLoggedIn }) => {
 
   return (
       <Router>
+        {isLoggedIn && <Navigation />}
         <Switch>
           {isLoggedIn ?
+              <>
                 <Route exact path="/">
                   <Home />
-                </Route> :
-                  <Route exact path="/">
-                    <Auth/>
-                  </Route>
+                </Route>
+                <Route exact path="/profile">
+                  <Profile />
+                </Route>
+                <Redirect from="*" to="/" />
+              </>
+              :
+              <>
+                <Route exact path="/">
+                  <Auth/>
+                </Route>
+                <Redirect from="*" to="/" />
+              </>
               }
         </Switch>
       </Router>
