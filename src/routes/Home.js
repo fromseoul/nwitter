461148b9adac6fd0339ed8d7a99297da5c3280af
Jungmin Kang <jmkang@goqual.com>
2021-12-10 09:38:31 +0900

    file upload and clear the photo

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -7,6 +7,7 @@ const Home = ({ userObj }) => {
 
   const [nweet, setNweet] = useState("");
   const [nweets, setNweets] = useState([]);
+  const [attachment, setAttachment] = useState();
 
   const getNweets = async () => {
     const dbNweets = await getDocs(collection(dbService, "nweets"));
@@ -45,11 +46,32 @@ const Home = ({ userObj }) => {
     setNweet(value);
   }
 
+  const onFileChange = (event) => {
+    const { target: { files }} = event;
+    const theFile = files[0];
+    const reader = new FileReader();
+    reader.onloadend = (finishedEvent) => {
+
+      const { currentTarget: { result }} = finishedEvent;
+      setAttachment(result);
+    }
+    reader.readAsDataURL(theFile);
+  }
+
+  const onClearAttachmentClick = () => setAttachment(null);
+
   return (
     <div>
       <form onSubmit={onSubmit}>
         <input value={nweet} onChange={onChange} type="text" placeholder="What's on your mind?" maxLength={120} />
+        <input type="file" accept="image/*" onChange={onFileChange} />
         <input type="submit" value="Nweet" />
+        {attachment &&
+        <div>
+          <img src={attachment} width="50px" height="50px" />
+          <button onClick={onClearAttachmentClick}>Clear</button>
+        </div>
+        }
       </form>
       <div>
         {nweets.map(nweet =>
